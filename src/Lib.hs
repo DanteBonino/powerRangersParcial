f31990adc13c03cbb856106320dfd6671f7fe4ed
DanteBonino <43711055+DanteBonino@users.noreply.github.com>
2023-06-03 19:38:42 -0300

    Punto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib () where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+--Punto 1:
+--a
+data Persona = Persona{
+    habilidades :: [String],
+    esBuena     :: Bool
+}
+
+dante :: Persona
+dante = Persona ["falta de atencion"] True
+
+--b
+data PowerRanger = PowerRanger{
+    color            :: String,
+    superHabilidades :: [String],
+    nivelDePelea     :: Int
+}
+powerRangerRojo :: PowerRanger
+powerRangerRojo = PowerRanger "rojo" ["superBailar"] 400
+
+--Punto 2:
